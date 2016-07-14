d8d47a10b68318ec1ae58f0d33cc5f0e343fe899
Davin Kevin <davinkevin@users.noreply.github.com>
2016-07-14 12:58:16 +0200

    feat(freebox): download with ffmpeg instead of mplayer

--- Cauet sur NRJ/Download/CauetDownloadFree.sh
+++ Cauet sur NRJ/Download/CauetDownloadFree.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 while [  true ]; do
 	Date=`date --date now '+%Y-%m-%d - %H-%M-%S'`
-	mplayer -dumpfile "/home/$USER/Cauet/$1 - $Date.mp2" -aid 1006 -dumpaudio "rtsp://mafreebox.free.fr/fbxtv_pub/stream?namespace=1&service=100011" >> "/home/$USER/scripts/$1.log" 2>&1
+	#mplayer -dumpfile "/home/$USER/Cauet/$1 - $Date.mp2" -aid 1006 -dumpaudio "rtsp://mafreebox.free.fr/fbxtv_pub/stream?namespace=1&service=100011" >> "/home/$USER/scripts/$1.log" 2>&1
+	/home/$USER/scripts/ffmpeg-static/ffmpeg -v "quiet" -i "rtsp://mafreebox.free.fr/fbxtv_pub/stream?namespace=1&service=100011" -acodec copy "/home/$USER/Cauet/$1 - $Date.mp2"
 done

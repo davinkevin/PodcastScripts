85d4b6931dc5243a5fd7d72f40de2184a56edf31
davinkevin <davin.kevin@gmail.com>
2017-04-09 14:04:00 +0200

    feat(deletion): support non existence of folder before deletion

--- Cauet sur NRJ/Download/deleteFolder.sh
+++ Cauet sur NRJ/Download/deleteFolder.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+   exit;
+fi
+
 numberOfDays=$1
 
 folder=/home/${USER}/Cauet/`date -d "${numberOfDays} days ago" '+%Y/%m/%d'`
 
-rm -rf $folder
+if [ -d "$folder" ]; then
+   rm -rf "$folder"
+fi
